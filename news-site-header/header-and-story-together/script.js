72bb0205633995c38855248ac013fe5196f819a1
bleehaus <bleehaus21@gmail.com>
2020-03-02 23:11:24 -0600

    Add files via upload

---
+++ news-site-header/header-and-story-together/script.js
@@ -0,0 +1,35 @@
+
+   //Script that adds Vue capability to #news-site-header 
+   //(This can, and should, really be in your own .js file) -->
+
+      new Vue({
+        el: '#menu_container',
+        data: {
+          menuIsOpen: false
+        },
+        methods: {
+          toggleOpen: function() {
+            this.menuIsOpen = !this.menuIsOpen
+          }
+        }
+      })
+
+      
+      var expandables = document.getElementsByClassName('expandable');
+      
+      for (let expandable of expandables) {
+        let triggerElement = expandable.querySelector('a');
+        triggerElement.addEventListener('click', function(event) {
+          event.preventDefault();
+          closeAllExcept(expandable);
+          expandable.classList.toggle('expanded');
+        })
+      }
+
+      function closeAllExcept(clickedExpandable) {
+        for (let expandable of expandables) {
+          if (expandable !== clickedExpandable) {
+            expandable.classList.remove('expanded')
+          }
+        }
+      }
